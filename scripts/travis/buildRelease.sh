c4fe156c2217a7d3dad86382cc6faef938dc85c7
David Edwards <david.edwards@alfresco.com>
2020-01-29 12:50:00 +0000

    REPO-4735 Create build script

---
+++ scripts/travis/buildRelease.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+releaseVersion=$1
+developmentVersion=$2
+echo scm_path
+if [ -z ${releaseVersion} ] || [ -z ${developmentVersion} ]; 
+    then echo skip  
+else   
+    echo "Do not skip"
+    # mvn --batch-mode \
+    # -DreleaseVersion=${releaseVersion} \
+    # -DdevelopmentVersion=${developmentVersion} \
+    # -Dbuild-number=${TRAVIS_BUILD_NUMBER} \
+    # -Dbuild-key=${bamboo.buildKey} \
+    # -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
+    # -Dscm-path=${scm_path} \
+    # "-Dversion.edition=${bamboo.version.edition}" \
+    # -DskipTests \
+    # "-Darguments=-DskipTests '-Dversion.edition=${bamboo.version.edition}' -Dbuild-number=${TRAVIS_BUILD_NUMBER} -Dbuild-key=${bamboo.buildKey} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}' -Dscm-path=${scm_path} " \   
+    # -Prelease \
+    # release:prepare release:perform  
+fi
