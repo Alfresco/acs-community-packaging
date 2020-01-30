ef4f20e2d69b27ae2dad1090d5131a4f16b8eb8b
David Edwards <david.edwards@alfresco.com>
2020-01-30 10:55:11 +0000

    Update buildRelease.sh
    replaced bamboo variables and added an automated version command

--- scripts/travis/buildRelease.sh
+++ scripts/travis/buildRelease.sh
@@ -6,19 +6,25 @@ developmentVersion=$2
 scm_path=$(mvn help:evaluate -Dexpression=project.scm.url -q -DforceStdout)
 
 if [ -z ${releaseVersion} ] || [ -z ${developmentVersion} ]; 
-    then echo skip  
+    then echo skip 
+    # mvn --batch-mode \
+    # -Dbuild-number=${TRAVIS_BUILD_NUMBER} \ \
+    # -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
+    # -Dscm-path=${scm_path} \
+    # -DskipTests \
+    # "-Darguments=-DskipTests -Dbuild-number=${TRAVIS_BUILD_NUMBER} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}' -Dscm-path=${scm_path} " \   
+    # -Prelease \
+    # release:prepare release:perform  
 else   
     echo "Do not skip"
     # mvn --batch-mode \
     # -DreleaseVersion=${releaseVersion} \
     # -DdevelopmentVersion=${developmentVersion} \
-    # -Dbuild-number=${TRAVIS_BUILD_NUMBER} \
-    # -Dbuild-key=${bamboo.buildKey} \
+    # -Dbuild-number=${TRAVIS_BUILD_NUMBER} \ \
     # -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
     # -Dscm-path=${scm_path} \
-    # "-Dversion.edition=${bamboo.version.edition}" \
     # -DskipTests \
-    # "-Darguments=-DskipTests '-Dversion.edition=${bamboo.version.edition}' -Dbuild-number=${TRAVIS_BUILD_NUMBER} -Dbuild-key=${bamboo.buildKey} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}' -Dscm-path=${scm_path} " \   
+    # "-Darguments=-DskipTests -Dbuild-number=${TRAVIS_BUILD_NUMBER} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}' -Dscm-path=${scm_path} " \   
     # -Prelease \
     # release:prepare release:perform  
 fi
