807a27f9cb9483f23a04d2bec1aeb10c7ba7460f
David Edwards <david.edwards@alfresco.com>
2020-03-06 10:12:02 +0000

    REPO-4735 Add alternative build script
    Added 2 script designed to extract the release and development versions required for the release process. V1 extracts all "commit variables" into and array and then uses a case statement to assign the variables. V2 extracts the desired variable directly into a variable.

---
+++ scripts/travis/mavenReleaseCommitVariablesV1.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+set -e
+
+commitMessage=$1
+# Lets create an array containing section of the commit message in the format [*=*] ie [devRelease=repo-4735] will be added as devRelease=repo-4735
+mapfile -t commitsVariables < <( echo $commitMessage | grep -Po '\[\K[^\]]*=[^\]]*(?=\])' )
+for i in ${commitsVariables[@]}
+do
+    variable=$(echo $i | grep -Po '=\K.*' )
+    variableName=$(echo $i | grep -Po '.*(?==)')
+    case  $variableName  in
+            "devRelease")      
+                developmentVersion="$variable"
+                ;;
+            "release")
+                releaseVersion="$variable"
+                ;;            
+            *)              
+    esac
+done
+
+scm_path=$(mvn help:evaluate -Dexpression=project.scm.url -q -DforceStdout)
+# Use full history for release
+git checkout -B "${TRAVIS_BRANCH}"
+# Add email to link commits to user
+git config user.email "${GIT_EMAIL}"
+
+if [ -z ${releaseVersion} ] || [ -z ${developmentVersion} ]; 
+    then echo "Please provide a Release and Development verison in the format <acs-version>-<additional-info> (6.3.0-EA or 6.3.0-SNAPSHOT)"
+         exit -1
+    # TODO: Set up continuous release. As of REPO-4735 the following is not required if release stage is manual
+    # mvn --batch-mode \
+    # -Dusername="${GIT_USERNAME}" \
+    # -Dpassword="${GIT_PASSWORD}" \
+    # -Dbuild-number=${TRAVIS_BUILD_NUMBER} \
+    # -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
+    # -Dscm-path=${scm_path} \
+    # -DscmCommentPrefix="[maven-release-plugin][skip ci]" \
+    # -DskipTests \
+    # "-Darguments=-DskipTests -Dbuild-number=${TRAVIS_BUILD_NUMBER} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}' -Dscm-path=${scm_path} " \
+    # -Prelease \
+    # release:prepare release:perform
+else   
+    mvn --batch-mode \
+    -Dusername="${GIT_USERNAME}" \
+    -Dpassword="${GIT_PASSWORD}" \
+    -DreleaseVersion=${releaseVersion} \
+    -DdevelopmentVersion=${developmentVersion} \
+    -Dbuild-number=${TRAVIS_BUILD_NUMBER} \
+    -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
+    -Dscm-path=${scm_path} \
+    -DscmCommentPrefix="[maven-release-plugin][skip ci]" \
+    -DskipTests \
+    "-Darguments=-DskipTests -Dbuild-number=${TRAVIS_BUILD_NUMBER} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}' -Dscm-path=${scm_path} " \
+    release:clean release:prepare release:perform \
+    -Prelease
+fi
