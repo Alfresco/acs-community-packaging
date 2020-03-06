d375e8d5aa5dbc530cc613849ed6fd7ecb628224
David Edwards <david.edwards@alfresco.com>
2020-03-06 10:27:35 +0000

    REPO-4735 add debuggin to new method
    [skipTests] message [trigger-release] [devRelease=6.3.0-SNAPSHOT] [release=6.3.0-A5][comRelease=202003] adding some nonsense to test the message -8y-18hrnfjif1][]1[]1r[1][f3][]3g[g]g[[]3[]g[4][]4]g4][3]455[5][6]34[]g[]g[42[r1r31443=1=3=411=3]

--- scripts/travis/mavenReleaseCommitVariablesV2.sh
+++ scripts/travis/mavenReleaseCommitVariablesV2.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-set -e
+set -ev
 
 commitMessage=$1
+echo "the commit message is $commitMessage"
 releaseVersion=$(echo $commitMessage | grep -Po '\[release[^\]]*=\K[^\]]*(?=\])')
 developmentVersion=$(echo $commitMessage | grep -Po '\[devRelease[^\]]*=\K[^\]]*(?=\])')
 
@@ -10,7 +11,8 @@ scm_path=$(mvn help:evaluate -Dexpression=project.scm.url -q -DforceStdout)
 git checkout -B "${TRAVIS_BRANCH}"
 # Add email to link commits to user
 git config user.email "${GIT_EMAIL}"
-
+echo "releaseVersion is assigned $releaseVersion"
+echo "developmentVersion is assigned $developmentVersion"
 if [ -z ${releaseVersion} ] || [ -z ${developmentVersion} ]; 
     then echo "Please provide a Release and Development verison in the format <acs-version>-<additional-info> (6.3.0-EA or 6.3.0-SNAPSHOT) by adding a commit message"
          exit -1
