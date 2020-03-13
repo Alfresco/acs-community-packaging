638c0f4c119dccd535d3ecf9a3983bed7351ffb5
David Edwards <david.edwards@alfresco.com>
2020-03-13 13:51:35 +0000

    REPO-4735 Remove excess comments, resolve TODO

--- scripts/travis/maven_release.sh
+++ scripts/travis/maven_release.sh
@@ -13,18 +13,6 @@ git config user.email "${GIT_EMAIL}"
 if [ -z ${releaseVersion} ] || [ -z ${developmentVersion} ]; 
     then echo "Please provide a Release and Development verison in the format <acs-version>-<additional-info> (6.3.0-EA or 6.3.0-SNAPSHOT)"
          exit -1
-    # TODO: Set up continuous release. As of REPO-4735 the following is not required if release stage is manual
-    # mvn --batch-mode \
-    # -Dusername="${GIT_USERNAME}" \
-    # -Dpassword="${GIT_PASSWORD}" \
-    # -Dbuild-number=${TRAVIS_BUILD_NUMBER} \
-    # -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
-    # -Dscm-path=${scm_path} \
-    # -DscmCommentPrefix="[maven-release-plugin][skip ci]" \
-    # -DskipTests \
-    # "-Darguments=-DskipTests -Dbuild-number=${TRAVIS_BUILD_NUMBER} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}' -Dscm-path=${scm_path} " \
-    # -Prelease \
-    # release:prepare release:perform
 else   
     mvn --batch-mode \
     -Dusername="${GIT_USERNAME}" \
