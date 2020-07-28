9b5149cc59b834543c39fef1fe594bdaea42f95c
Cezar.Leahu <cezar.leahu@ness.com>
2020-07-29 02:29:15 +0300

    ACS-457: Build linkage, tagging and release
    - update and propagate branch version numbers during the build

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -6,23 +6,23 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 [ "${TRAVIS_PULL_REQUEST}" = "false" ] && BRANCH="${TRAVIS_BRANCH}" || BRANCH="${TRAVIS_PULL_REQUEST_BRANCH}"
 
 # if BRANCH is not 'master' or 'release/' AND if it exists in the upstream project
+UPSTREAM_REPO="github.com/Alfresco/alfresco-community-repo.git"
 if ! [[ "${BRANCH}" =~ ^master$\|^release/.+$ ]] && \
-  git ls-remote --exit-code --heads \
-  https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/Alfresco/alfresco-community-repo.git \
-  "${BRANCH}" ; then
-
+  git ls-remote --exit-code --heads https://${GIT_USERNAME}:${GIT_PASSWORD}@${UPSTREAM_REPO} "${BRANCH}" ; then
   # clone and build the upstream repository
   pushd ..
 
   rm -rf alfresco-community-repo
-  git clone -b ${BRANCH} https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/Alfresco/alfresco-community-repo.git
+  git clone -b ${BRANCH} https://${GIT_USERNAME}:${GIT_PASSWORD}@${UPSTREAM_REPO}
   cd alfresco-community-repo
-  # todo set a custom branch version before the SNAPSHOT build?
-  mvn -B -V clean install -DskipTests -PcommunityDocker
+  mvn -B -V -q clean install -DskipTests -PcommunityDocker
+  UPSTREAM_VERSION=$(mvn -B -q help:evaluate -Dexpression=project.version -DforceStdout)
 
   popd
-  # todo update the parent dependency dependency so that it uses the locally-build SNAPSHOT version?
 fi
 
-mvn -B -V -q install -DskipTests -Dmaven.javadoc.skip=true "-Dversion.edition=${VERSION_EDITION}" -PcommunityDocker
+# update the parent dependency if needed
+[ ! -z ${UPSTREAM_VERSION} ] && mvn -B versions:update-parent "-DparentVersion=(0,${UPSTREAM_VERSION}]" versions:commit
 
+# Build the current project also
+mvn -B -V -q install -DskipTests -Dmaven.javadoc.skip=true "-Dversion.edition=${VERSION_EDITION}" -PcommunityDocker
