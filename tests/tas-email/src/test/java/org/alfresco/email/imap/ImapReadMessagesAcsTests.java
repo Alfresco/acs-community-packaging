62aac576e3f886f2fc8cf8a113a1f2e36b44e610
Alan Davis <alan.davis@alfresco.com>
2020-07-29 22:14:53 +0100

    email and webdav de-dup

---
+++ tests/tas-email/src/test/java/org/alfresco/email/imap/ImapReadMessagesAcsTests.java
@@ -0,0 +1,28 @@
+package org.alfresco.email.imap;
+
+import org.alfresco.utility.model.*;
+import org.alfresco.utility.testrail.ExecutionType;
+import org.alfresco.utility.testrail.annotation.TestRail;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ImapReadMessagesAcsTests extends EmailTest
+{
+    @BeforeClass(alwaysRun=true)
+    public void dataPreparation() throws Exception
+    {
+        testUser = dataUser.createRandomTestUser();
+        testSite = dataSite.usingUser(testUser).createIMAPSite();
+        adminUser = dataUser.getAdminUser();
+        adminSite = dataSite.usingAdmin().createIMAPSite();
+    }
+
+    @TestRail(section = { TestGroup.PROTOCOLS, TestGroup.IMAP }, executionType = ExecutionType.REGRESSION,
+            description = "Verify that site manager can see wiki pages via IMAP")
+    @Test(groups = { TestGroup.PROTOCOLS, TestGroup.IMAP, TestGroup.CORE })
+    public void siteManagerCanViewWikiPages() throws Exception
+    {
+        dataWiki.usingUser(testUser).usingSite(testSite).createRandomWiki();
+        imapProtocol.authenticateUser(testUser).usingSiteWikiContainer(testSite).assertThat().countMessagesIs(1);
+    }
+}
