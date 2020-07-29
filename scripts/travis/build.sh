4cf46c3d5dd9f8dfcbc1a2c5873313c6cfdffd35
Cezar.Leahu <cezar.leahu@ness.com>
2020-07-29 22:44:27 +0300

    ACS-457: Build linkage, tagging and release
    - handle upstream versions in chained feature branch builds

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -8,12 +8,12 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 # if BRANCH is not 'master' or 'release/' AND if it exists in the upstream project
 UPSTREAM_REPO="github.com/Alfresco/alfresco-community-repo.git"
 if ! [[ "${BRANCH}" =~ ^master$\|^release/.+$ ]] && \
-  git ls-remote --exit-code --heads https://${GIT_USERNAME}:${GIT_PASSWORD}@${UPSTREAM_REPO} "${BRANCH}" ; then
+  git ls-remote --exit-code --heads "https://${GIT_USERNAME}:${GIT_PASSWORD}@${UPSTREAM_REPO}" "${BRANCH}" ; then
   # clone and build the upstream repository
   pushd ..
 
   rm -rf alfresco-community-repo
-  git clone -b ${BRANCH} https://${GIT_USERNAME}:${GIT_PASSWORD}@${UPSTREAM_REPO}
+  git clone -b "${BRANCH}" "https://${GIT_USERNAME}:${GIT_PASSWORD}@${UPSTREAM_REPO}"
   cd alfresco-community-repo
   mvn -B -V -q clean install -DskipTests -PcommunityDocker
   UPSTREAM_VERSION=$(mvn -B -q help:evaluate -Dexpression=project.version -DforceStdout)
@@ -22,7 +22,7 @@ if ! [[ "${BRANCH}" =~ ^master$\|^release/.+$ ]] && \
 fi
 
 # update the parent dependency if needed
-[ ! -z ${UPSTREAM_VERSION} ] && mvn -B versions:update-parent "-DparentVersion=(0,${UPSTREAM_VERSION}]" versions:commit
+[ -n "${UPSTREAM_VERSION}" ] && mvn -B versions:update-parent "-DparentVersion=(0,${UPSTREAM_VERSION}]" versions:commit
 
 # Build the current project also
 mvn -B -V -q install -DskipTests -Dmaven.javadoc.skip=true "-Dversion.edition=${VERSION_EDITION}" -PcommunityDocker
