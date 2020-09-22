c494becbf6a203afd99ae42763af8a2467d26828
Alan Davis <alan.davis@alfresco.com>
2020-09-22 16:36:58 +0100

    Test in messages should say 7.0.0 now

--- scripts/travis/copy_to_release_bucket.sh
+++ scripts/travis/copy_to_release_bucket.sh
@@ -8,7 +8,7 @@ set -vex
 #
 
 if [ -z "${RELEASE_VERSION}" ]; then
-  echo "Please provide a RELEASE_VERSION in the format <acs-version>-<additional-info> (6.3.0-EA or 6.3.0-SNAPSHOT)"
+  echo "Please provide a RELEASE_VERSION in the format <acs-version>-<additional-info> (7.0.0-EA or 7.0.0-SNAPSHOT)"
   exit 1
 fi
 
--- scripts/travis/maven_release.sh
+++ scripts/travis/maven_release.sh
@@ -6,7 +6,7 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 
 
 if [ -z "${RELEASE_VERSION}" ] || [ -z "${DEVELOPMENT_VERSION}" ]; then
-  echo "Please provide a Release and Development version in the format <acs-version>-<additional-info> (6.3.0-EA or 6.3.0-SNAPSHOT)"
+  echo "Please provide a Release and Development version in the format <acs-version>-<additional-info> (7.0.0-EA or 7.0.0-SNAPSHOT)"
   exit 1
 fi
 
