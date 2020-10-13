345af13604224a40c6c688ab3bc50069d4e3ac8d
DenisGabriela <Denis.Ungureanu1@ness.com>
2020-10-13 15:46:54 +0300

    MNT-21871 : [Security] Multiple jackson-databind vulnerabilities

--- tests/scripts/wait-for-alfresco-start.sh
+++ tests/scripts/wait-for-alfresco-start.sh
@@ -27,5 +27,9 @@ if (("$COUNTER" < "$TIMEOUT")) ; then
 else
    echo "Waited $COUNTER seconds"
    echo "Alfresco Could not start in time."
+   docker ps -a	
+   ALFCONTAINER=`docker ps -a | grep _alfresco | awk '{ print $1 }'`
+   echo "Last 200 lines from alfresco.log on container $ALFCONTAINER:"
+   docker logs --tail=200 $ALFCONTAINER
    exit 1
 fi
