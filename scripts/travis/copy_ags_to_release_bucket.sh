d8b1c5f543c7cb94eab14430227c3fd684383ba1
alandavis <alan.davis@alfresco.com>
2021-10-01 07:48:28 +0100

    prep work 7.1.0 -> 7.1.1

--- scripts/travis/copy_ags_to_release_bucket.sh
+++ scripts/travis/copy_ags_to_release_bucket.sh
@@ -8,7 +8,7 @@ set -vex
 #
 
 if [ -z "${RELEASE_VERSION}" ]; then
-  echo "Please provide a RELEASE_VERSION in the format <acs-version>-<additional-info> (7.1.0-EA or 7.1.0-SNAPSHOT)"
+  echo "Please provide a RELEASE_VERSION in the format <acs-version>-<additional-info> (7.1.1-EA or 7.1.1-SNAPSHOT)"
   exit 1
 fi
 
