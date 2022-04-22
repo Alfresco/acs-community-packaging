dfe6a27785d44f34dbea4db95746f670f08cb554
alandavis <alan.davis@alfresco.com>
2022-04-22 13:24:36 +0100

    ACS-2768 tidy up start-compose.sh

--- scripts/dev/start-compose.sh
+++ scripts/dev/start-compose.sh
@@ -1,5 +1,5 @@
 set -x
-TRANSFORMERS_TAG=$(mvn help:evaluate -Dexpression=dependency.alfresco-transform-core.version -q -DforceStdout)
+export TRANSFORMERS_TAG=$(mvn help:evaluate -Dexpression=dependency.alfresco-transform-core.version -q -DforceStdout)
 
 # .env files are picked up from project directory correctly on docker-compose 1.23.0+
-TRANSFORMERS_TAG=${TRANSFORMERS_TAG} docker-compose -f acs-packaging/dev/docker-compose.yml up
+docker-compose -f acs-packaging/dev/docker-compose.yml up
