0041f256190be41ef4db0c53c9d9044c82fd4915
Domenico Sibilio <domenicosibilio@gmail.com>
2022-05-04 13:00:17 +0200

    Fix broken 'envUp' alias

--- scripts/dev/start-compose.sh
+++ scripts/dev/start-compose.sh
@@ -1,5 +1,5 @@
 set -x
-export TRANSFORMERS_TAG=$(mvn help:evaluate -Dexpression=dependency.alfresco-transform-core.version -q -DforceStdout)
+export TRANSFORMERS_TAG=$(mvn -f acs-community-packaging/pom.xml help:evaluate -Dexpression=dependency.alfresco-transform-core.version -q -DforceStdout)
 
 # .env files are picked up from project directory correctly on docker-compose 1.23.0+
-docker-compose -f acs-packaging/dev/docker-compose.yml up
+docker-compose -f acs-community-packaging/dev/docker-compose.yml up
