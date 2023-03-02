be1d5982395086461df39d895ab0d1c18f2da273
Damian.Ujma@hyland.com <Damian.Ujma@hyland.com>
2023-03-02 12:33:09 +0100

    ACS-4140 Push docker images to local repository

--- scripts/dev/prepare_buildx.sh
+++ scripts/dev/prepare_buildx.sh
@@ -1,44 +1,21 @@
 #!/usr/bin/env bash
 
-LOCAL_REGISTRY="${1}"
-BASE_IMAGE="${2}"
-BUILDER_NAME="${3}"
-TARGET_REGISTRY="${4}"
-TARGET_IMAGE="${5}"
-IMAGE_TAG="${6}"
-SLEEP_SECONDS=0
+BUILDER_NAME="${1}"
+TARGET_REGISTRY="${2}"
+TARGET_IMAGE="${3}"
+IMAGE_TAG="${4}"
 
-if [ "${LOCAL_REGISTRY}" != "127.0.0.1:5000" ]; then
-  echo "The local registry is not set to 127.0.0.1:5000. Skipping image push."
-else
-   #Run a local registry server
-   docker run -d -p 5000:5000 --restart=always --name registry registry:2
+#Create a `docker-container` builder with host networking and required flags (quay.io)
+docker --config target/docker/"${TARGET_REGISTRY}"/"${TARGET_IMAGE}"/"${IMAGE_TAG}"/docker \
+buildx create --use --name "${BUILDER_NAME}" --driver-opt network=host \
+--buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'
 
-   #Push base image to the local repository
-   docker tag "${BASE_IMAGE}" "${LOCAL_REGISTRY}"/"${BASE_IMAGE}"
-   while [ "$( docker container inspect -f '{{.State.Running}}' registry )" != "true" ] && [ $SLEEP_SECONDS -lt 600 ]
-   do
-    ((SLEEP_SECONDS++))
-    sleep 1
-   done
+#Create a `docker-container` builder with host networking and required flags (docker.io)
+docker --config target/docker/"${TARGET_IMAGE}"/"${IMAGE_TAG}"/docker \
+buildx create --use --name "${BUILDER_NAME}" --driver-opt network=host \
+--buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'
 
-   PUSH_TAG_ATTEMPTS=6
-   RESULT=1
-   while [ $RESULT -eq 1 ] && [ $PUSH_TAG_ATTEMPTS -gt 0 ]
-   do
-    docker push "${LOCAL_REGISTRY}"/"${BASE_IMAGE}"
-    RESULT=$?
-    ((PUSH_TAG_ATTEMPTS--))
-    sleep 10
-   done
-
-   #Create a `docker-container` builder with host networking and required flags (quay.io)
-   docker --config target/docker/"${TARGET_REGISTRY}"/"${TARGET_IMAGE}"/"${IMAGE_TAG}"/docker \
-   buildx create --use --name "${BUILDER_NAME}" --driver-opt network=host \
-   --buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'
-
-   #Create a `docker-container` builder with host networking and required flags (docker.io)
-   docker --config target/docker/"${TARGET_IMAGE}"/"${IMAGE_TAG}"/docker \
-   buildx create --use --name "${BUILDER_NAME}" --driver-opt network=host \
-   --buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'
-fi
+#Create a `docker-container` builder with host networking and required flags (local registry)
+docker --config target/docker/127.0.0.1/5000/"${TARGET_IMAGE}"/"${IMAGE_TAG}"/docker \
+buildx create --use --name "${BUILDER_NAME}" --driver-opt network=host \
+--buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'
