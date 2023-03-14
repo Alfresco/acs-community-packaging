c3286dbcbf624726e4b4863512a0671a8eccd813
Wojtek Świętoń <wojtek.swieton@gmail.com>
2023-03-14 16:33:26 +0100

    ACS-4140 Produce Multi-Arch Docker images (#1508)
    * ACS-4140 Changes in pom and Dockerfile for alfresco-content-repository-community image
    
    * ACS-4140 Changes in pom and Dockerfile for alfresco-governance-share-community and alfresco-governance-repository-community
    
    * ACS-4140 Add docker image build mode option in build.sh
    
    * ACS-4140 Fix in prepare_buildx.sh to not duplicate image.tag
    
    * ACS-4139 Fix intermittent failures
    
    * ACS-4139 Remove echo
    
    * ACS-4140 Refactor build.sh
    
    * ACS-4140 Added missing <BASE_IMAGE> argument
    
    * ACS-4140 Push docker images to local repository
    
    * ACS-4140 Add 'build-multiarch-docker-images' profile to 'share-community-docker'
    
    * ACS-4140 Use build for 'push-docker-images' and 'release' profiles
    
    * ACS-4140 Remove comments
    
    * ACS-4140 Add missing properties
    
    * ACS-4140 Inherit required properties
    
    * ACS-4140 Change the 'build-multiarch-docker-image' profile phase to package
    
    * Revert "ACS-4140 Change the 'build-multiarch-docker-image' profile phase to package"
    
    This reverts commit 38781abc9a5243c63cf2ee6af9aac155ab0d656c.
    
    * ACS-4140 Bump community repo/share versions
    
    ---------
    
    Co-authored-by: Damian.Ujma@hyland.com <Damian.Ujma@hyland.com>

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -6,6 +6,28 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 
 source "$(dirname "${BASH_SOURCE[0]}")/build_functions.sh"
 
+usage() {
+    echo "Builds the upstream projects first, then the current one." 1>&2;
+    echo 1>&2;
+    echo "Usage: $0 [-m]" 1>&2;
+    echo "  -m: Flag to build Docker images with multi-architecture" 1>&2;
+    echo "  -h: Display the usage information" 1>&2;
+    exit 1;
+}
+
+while getopts "mh" option; do
+   case $option in
+      m)
+        DOCKER_BUILD_PROFILE=build-multiarch-docker-images
+        ;;
+      h)
+        usage
+        ;;
+   esac
+done
+
+BUILD_PROFILE=${DOCKER_BUILD_PROFILE:-build-docker-images}
+
 COM_DEPENDENCY_VERSION="$(retrievePomProperty "dependency.alfresco-community-repo.version")"
 REPO_IMAGE=$([[ "${COM_DEPENDENCY_VERSION}" =~ ^.+-SNAPSHOT$ ]] && echo "-Drepo.image.tag=latest" || echo)
 
@@ -32,9 +54,9 @@ UPSTREAM_REPO="github.com/Alfresco/alfresco-community-repo.git"
 # Search, checkout and build the same branch on the upstream project in case of SNAPSHOT dependencies
 # Otherwise, checkout the upstream tag and build its Docker image (use just "mvn package", without "mvn install")
 if [[ "${COM_DEPENDENCY_VERSION}" =~ ^.+-SNAPSHOT$ ]] ; then
-  pullAndBuildSameBranchOnUpstream "${UPSTREAM_REPO}" "-Pbuild-docker-images -Pags -Dlicense.failOnNotUptodateHeader=true"
+  pullAndBuildSameBranchOnUpstream "${UPSTREAM_REPO}" "-P$BUILD_PROFILE -Pags -Dlicense.failOnNotUptodateHeader=true"
 else
-  pullUpstreamTagAndBuildDockerImage "${UPSTREAM_REPO}" "${COM_DEPENDENCY_VERSION}" "-Pbuild-docker-images -Pags -Dlicense.failOnNotUptodateHeader=true"
+  pullUpstreamTagAndBuildDockerImage "${UPSTREAM_REPO}" "${COM_DEPENDENCY_VERSION}" "-P$BUILD_PROFILE -Pags -Dlicense.failOnNotUptodateHeader=true"
 fi
 
 SHARE_DEPENDENCY_VERSION="$(retrievePomProperty "dependency.alfresco-community-share.version")"
@@ -55,13 +77,13 @@ fi
 SHARE_UPSTREAM_REPO="github.com/Alfresco/alfresco-community-share.git"
 # Checkout the upstream share project (tag or branch; + build if the latter)
 if [[ "${SHARE_DEPENDENCY_VERSION}" =~ ^.+-SNAPSHOT$ ]] ; then
-  pullAndBuildSameBranchOnUpstream "${SHARE_UPSTREAM_REPO}" "-Pbuild-docker-images -Pags -Dlicense.failOnNotUptodateHeader=true -Ddocker.quay-expires.value=NEVER ${REPO_IMAGE} -Ddependency.alfresco-community-repo.version=${COM_DEPENDENCY_VERSION}"
+  pullAndBuildSameBranchOnUpstream "${SHARE_UPSTREAM_REPO}" "-P$BUILD_PROFILE -Pags -Dlicense.failOnNotUptodateHeader=true -Ddocker.quay-expires.value=NEVER ${REPO_IMAGE} -Ddependency.alfresco-community-repo.version=${COM_DEPENDENCY_VERSION}"
 else
-  pullUpstreamTagAndBuildDockerImage "${SHARE_UPSTREAM_REPO}" "${SHARE_DEPENDENCY_VERSION}" "-Pbuild-docker-images -Pags -Dlicense.failOnNotUptodateHeader=true -Ddocker.quay-expires.value=NEVER -Ddependency.alfresco-community-repo.version=${COM_DEPENDENCY_VERSION}"
+  pullUpstreamTagAndBuildDockerImage "${SHARE_UPSTREAM_REPO}" "${SHARE_DEPENDENCY_VERSION}" "-P$BUILD_PROFILE -Pags -Dlicense.failOnNotUptodateHeader=true -Ddocker.quay-expires.value=NEVER -Ddependency.alfresco-community-repo.version=${COM_DEPENDENCY_VERSION}"
 fi
 
 # Build the current project
-mvn -B -ntp -V -q install -DskipTests -Dmaven.javadoc.skip=true -Pbuild-docker-images -Pags ${REPO_IMAGE} ${SHARE_IMAGE}
+mvn -B -ntp -V -q install -DskipTests -Dmaven.javadoc.skip=true -P$BUILD_PROFILE -Pags ${REPO_IMAGE} ${SHARE_IMAGE}
 
 
 popd
